7af32b110d01fa044820536a10a607e34efd8fae
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2021-06-30 18:19:07 +0100

    travis master -> main

--- travis-build.sh
+++ travis-build.sh
@@ -6,8 +6,8 @@ set -e
 
 cabal new-build all --enable-tests
 cabal new-test all --enable-tests
-git fetch origin master:origin/master
-git rebase origin/master --exec "cabal new-build all --enable-tests"
+git fetch origin main:origin/main
+git rebase origin/main --exec "cabal new-build all --enable-tests"
 
 # Stack
 
@@ -19,5 +19,5 @@ curl -sSL https://get.haskellstack.org/ | sh
 # build project with stack
 stack --version
 stack build --system-ghc --test
-git fetch origin master:origin/master
-git rebase origin/master --exec "stack build --system-ghc --test"
+git fetch origin main:origin/main
+git rebase origin/main --exec "stack build --system-ghc --test"
